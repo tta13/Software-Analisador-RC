713e447de2084bfa137dd56391ddd131525a1372
Tales Alves <5470324-tta13@users.noreply.gitlab.com>
2022-01-24 12:24:48 -0300

    Adding loading spinner to match stats page

--- frontend/src/app/match-stats/match-stats.component.ts
+++ frontend/src/app/match-stats/match-stats.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatchData } from './match-data';
 import { ActivatedRoute } from '@angular/router'
 import { MatchStatsService } from './match-stats.service';
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Component({
   selector: 'app-match-stats',
@@ -11,18 +12,28 @@ import { MatchStatsService } from './match-stats.service';
 export class MatchStatsComponent implements OnInit {
 
   matchData: MatchData = new MatchData;
+  loading: BehaviorSubject<boolean> = new BehaviorSubject(false as boolean)
 
   constructor(private statsService: MatchStatsService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.paramMap
-    .subscribe(params => {
-
-      this.statsService.getMatchStats(params.get('id') as string)
-        .subscribe((data) => {
-          this.matchData = JSON.parse(data);
-        });
-    });
+    this.getMatchStats();
   }
 
+  private getMatchStats() {
+    this.loading.next(true);
+
+    this.route.paramMap
+      .subscribe(params => {
+        this.statsService.getMatchStats(params.get('id') as string)
+          .subscribe({
+            next: (data) => this.matchData = JSON.parse(data),
+            complete: () => this.loading.next(false),
+          });
+      });
+  }
+
+  isLoading(): Observable<boolean> {
+    return this.loading.asObservable();
+  }
 }
